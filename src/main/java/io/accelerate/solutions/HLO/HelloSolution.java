91934fed30c4246cc0d0dea330e3c4de5916251f
sbx_user1051 <sbx_user1051@169.254.36.129>
2025-10-05 21:57:49 +0000

    Sun Oct 05 21:57:49 UTC 2025

--- src/main/java/io/accelerate/solutions/HLO/HelloSolution.java
+++ src/main/java/io/accelerate/solutions/HLO/HelloSolution.java
@@ -4,8 +4,19 @@ import io.accelerate.runner.SolutionNotImplementedException;
 
 public class HelloSolution {
     public String hello(String friendName) {
+
         final StringBuilder helloWorld = new StringBuilder();
-        helloWorld.append("Hello, World!");
+
+        if (friendName == null || friendName.isEmpty()) {
+            helloWorld.append("Hello, World!");
+            return helloWorld.toString();
+        }
+
+        helloWorld.append("Hello, ");
+        helloWorld.append(friendName);
+        helloWorld.append("!");
+
         return helloWorld.toString();
     }
 }
+
