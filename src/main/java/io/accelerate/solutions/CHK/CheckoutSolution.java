52b2e654c8ff2126c878cf6c3c97033ede83cb66
sbx_user1051 <sbx_user1051@169.254.36.129>
2025-10-05 22:11:23 +0000

    Sun Oct 05 22:11:23 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -2,21 +2,38 @@ package io.accelerate.solutions.CHK;
 
 import io.accelerate.runner.SolutionNotImplementedException;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CheckoutSolution {
     public Integer checkout(String skus) {
-
-
-        boolean containsValidSkus = skus.matches("[ABCD]*");
+        final boolean containsValidSkus = skus.matches("[ABCD]*");
 
         if(!containsValidSkus)  {
             return -1;
         }
 
-        skus.toCharArray();
+        final HashMap<Character, Integer> checkOutMap = new HashMap<>();
 
+        for (char currentSku :skus.toCharArray()) {
+            if (checkOutMap.containsKey(currentSku)) {
+                 checkOutMap.put(currentSku, checkOutMap.get(currentSku) + 1);
+                 continue;
+            }
 
+            checkOutMap.put(currentSku, 1);
+        };
+
+        for (Map.Entry<Character, Integer> currentSkus: checkOutMap.entrySet()) {
+            if (){
+
+            }
+        }
+
+        return null;
     }
 }
+
 
 
 
