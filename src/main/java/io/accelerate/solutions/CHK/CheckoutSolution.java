99a5c9a9e10dde568108b25ca3b22508d09f7e1a
sbx_user1051 <sbx_user1051@169.254.36.129>
2025-10-05 22:05:23 +0000

    Sun Oct 05 22:05:23 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -4,10 +4,13 @@ import io.accelerate.runner.SolutionNotImplementedException;
 
 public class CheckoutSolution {
     public Integer checkout(String skus) {
-        
+
+        skus.matches("[ABCD]*");
+
         skus.toCharArray();
 
 
     }
 }
+
 
