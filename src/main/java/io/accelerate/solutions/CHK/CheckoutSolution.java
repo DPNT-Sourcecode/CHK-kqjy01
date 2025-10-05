a64d99368701344474c18901de09454cefb55b2f
sbx_user1051 <sbx_user1051@169.254.36.129>
2025-10-05 22:32:53 +0000

    Sun Oct 05 22:32:53 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -1,7 +1,5 @@
 package io.accelerate.solutions.CHK;
 
-import io.accelerate.runner.SolutionNotImplementedException;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,40 +11,50 @@ public class CheckoutSolution {
             return -1;
         }
 
-        final HashMap<Character, Integer> checkOutMap = new HashMap<>();
-
-        for (char currentSku :skus.toCharArray()) {
-            if (checkOutMap.containsKey(currentSku)) {
-                 checkOutMap.put(currentSku, checkOutMap.get(currentSku) + 1);
-                 continue;
-            }
-
-            checkOutMap.put(currentSku, 1);
-        };
+        final HashMap<Character, Integer> checkOutMap = getSkuMap(skus); ;
 
         int checkOutValue = 0;
 
         for (Map.Entry<Character, Integer> currentSkus: checkOutMap.entrySet()) {
-            switch (currentSkus.getKey()){
-                case 'A':
-                    checkOutValue = checkOutValue + ((currentSkus.getValue() / 3) * 130) + (currentSkus.getValue() % 3 * 50);
-                    break;
-                case 'B':
-                    checkOutValue = checkOutValue + ((currentSkus.getValue() / 2) * 45) + (currentSkus.getValue() % 2 * 30);
-                    break;
-                case 'C':
-                    checkOutValue = checkOutValue + (20 * currentSkus.getValue());
-                    break;
-                case 'D':
-                    checkOutValue = checkOutValue + (15 * currentSkus.getValue());
-                    break;
-
-            }
+            checkOutValue = computValue(currentSkus, checkOutValue);
         }
 
         return checkOutValue;
     }
+
+    private static HashMap<Character, Integer> getSkuMap(String skus) {
+        final HashMap<Character, Integer> checkOutMap = new HashMap<>();
+
+        for (char currentSku : skus.toCharArray()) {
+            if (checkOutMap.containsKey(currentSku)) {
+                 checkOutMap.put(currentSku, checkOutMap.get(currentSku) + 1);
+                 continue;
+            }
+            checkOutMap.put(currentSku, 1);
+        };
+
+        return checkOutMap;
+    }
+
+    private static int computValue(Map.Entry<Character, Integer> currentSkus, int checkOutValue) {
+        switch (currentSkus.getKey()){
+            case 'A':
+                checkOutValue = checkOutValue + ((currentSkus.getValue() / 3) * 130) + (currentSkus.getValue() % 3 * 50);
+                break;
+            case 'B':
+                checkOutValue = checkOutValue + ((currentSkus.getValue() / 2) * 45) + (currentSkus.getValue() % 2 * 30);
+                break;
+            case 'C':
+                checkOutValue = checkOutValue + (20 * currentSkus.getValue());
+                break;
+            case 'D':
+                checkOutValue = checkOutValue + (15 * currentSkus.getValue());
+                break;
+        }
+        return checkOutValue;
+    }
 }
+
 
 
 
